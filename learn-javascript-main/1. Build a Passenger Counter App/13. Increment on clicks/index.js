2e73306a68f6cc0c32ea276f64f202a41749202f
nitish-pardal <nitishpardal@gmail.com>
2023-10-31 17:21:51 +0530

    31-10-2023

--- learn-javascript-main/1. Build a Passenger Counter App/13. Increment on clicks/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/13. Increment on clicks/index.js
@@ -8,7 +8,9 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
-
-
-
+let count =0;
+document.getElementById('increment-btn').addEventListener('click',() =>{
+  count+=1;
+  console.log(count);
+});
 
