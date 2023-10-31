40cd2ecee8bc349c81985b53f4278db0ead023f5
nitish-pardal <nitishpardal@gmail.com>
2023-10-31 16:19:51 +0530

    31-10-2023

--- learn-javascript-main/1. Build a Passenger Counter App/12. Write a function that increments/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -3,7 +3,12 @@ let lapsCompleted = 0
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
 
+function increment(){
+  lapsCompleted += 1;
+  console.log(lapsCompleted);
+}
 
 
-
-console.log(lapsCompleted)
+increment();
+increment();
+increment();
