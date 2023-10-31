135dbc2a337c7d36180ddaca0bbeddebc8872e88
nitish-pardal <nitishpardal@gmail.com>
2023-10-31 20:46:58 +0530

    31-10-2023

--- learn-javascript-main/1. Build a Passenger Counter App/17. Create the save button/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/17. Create the save button/index.js
@@ -4,8 +4,16 @@ let count = 0
 function increment() {
     count = count + 1
     countEl.innerText = count
+    save();
 }
 
 // 1. Create a function, save(), which logs out the count when it's called
+document.getElementById('save-btn').addEventListener('click',()=>{
+    save();
+});
+
+function save(){
+    console.log(count);
+}
 
 
