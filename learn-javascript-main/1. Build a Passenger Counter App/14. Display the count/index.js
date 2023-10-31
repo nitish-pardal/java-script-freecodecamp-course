4b6e92508099140f98f63ef760917661882f4ba9
nitish-pardal <nitishpardal@gmail.com>
2023-10-31 17:34:35 +0530

    31-10-2023

--- learn-javascript-main/1. Build a Passenger Counter App/14. Display the count/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/14. Display the count/index.js
@@ -1,12 +1,13 @@
-// document.getElementById("count").innerText = 5
+//
 
 // change the count-el in the HTML to reflect the new count
 
 let count = 0
-
+let countEl=document.getElementById('count-el');
 function increment() {
-    count = count + 1
-    console.log(count)
+    count = count + 1;
+     countEl.innerText =count;
+    console.log(count);
 }
 
 
