3dd153a05dc669a0e97c5b05ac32ae36d50e8e71
nitish-pardal <nitishpardal@gmail.com>
2023-10-31 14:04:33 +0530

    31-10-2023

--- learn-javascript-main/1. Build a Passenger Counter App/05. Basic mathematical operations/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/05. Basic mathematical operations/index.js
@@ -6,7 +6,10 @@
 // let count = firstBatch + secondBatch
 // console.log(count)
 
-
+const myAge=23;
+const humanDogRatio=7;
+let myDogAge=myAge*humanDogRatio;
+console.log(myDogAge);
 // 1. Create two variables, myAge and humanDogRatio
 // 2. Multiply the two together and store the result in myDogAge 
 // 3. Log myDogAge to the console
