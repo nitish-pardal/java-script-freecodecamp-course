e984b1c4dfef0a974990c54ca737d8276a9ac2b7
nitish-pardal <nitishpardal@gmail.com>
2023-10-31 15:01:46 +0530

    31-10-2023

--- learn-javascript-main/1. Build a Passenger Counter App/08. The onclick event listener/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/08. The onclick event listener/index.js
@@ -9,6 +9,11 @@
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
+let count=0;
+document.getElementById('increment-btn').addEventListener('click',()=>{
+  count+=1;
+ document.getElementById('count-el').innerHTML=count;
+});
 
 
 
