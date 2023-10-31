8c609dc64966a3904d7937121cadfea0a2d83872
nitish-pardal <nitishpardal@gmail.com>
2023-10-31 20:36:07 +0530

    31-10-2023

--- learn-javascript-main/1. Build a Passenger Counter App/16. Display the count with innerText/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/16. Display the count with innerText/index.js
@@ -1,9 +1,12 @@
 // grab the count-el element, store it in a countEl variable
-let count = 0
-
-function increment() {
-    count = count + 1
-    // set countEl's innerText to the count
+ let countEl =document.getElementById('increment-btn');
+ countEl.addEventListener('click', ()=>{
+    increment();
+ })
+ let count=0;
+function increment(){
+    
+    count+=1;
+    document.getElementById('count-el').innerText=count;
 }
-
 
