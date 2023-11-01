2db2b3b78d06e6ca726b3f1cb7665447ccc07064
nitish-pardal <nitishpardal@gmail.com>
2023-11-01 11:38:21 +0530

    1-11-2023

--- learn-javascript-main/1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
@@ -1,6 +1,6 @@
 let welcomeEl = document.getElementById("welcome-el")
 
-let name = "Per Harald Borgen"
+let name = "Nitish Pardal"
 let greeting = "Welcome back "
 
 welcomeEl.innerText = greeting + name
@@ -8,4 +8,5 @@ welcomeEl.innerText = greeting + name
 // Add an emoji to the end!
 // WRITE YOUR CODE BELOW HERE
 // HINT: count = count + 1
+welcomeEl.innerText += "👋";
 
