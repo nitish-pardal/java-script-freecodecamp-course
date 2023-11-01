0086f1074067b18394e9728f779b38023fdccd4c
nitish-pardal <nitishpardal@gmail.com>
2023-11-01 16:54:06 +0530

    01-11-2023

--- learn-javascript-main/3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ learn-javascript-main/3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -2,3 +2,7 @@
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
+let firstCard =3;
+let secondCard = 6;
+let sum= firstCard+secondCard;
+
--- learn-javascript-main/3. Build a Blackjack game/03. If...else conditionals/index.js
+++ learn-javascript-main/3. Build a Blackjack game/03. If...else conditionals/index.js
@@ -1,4 +1,13 @@
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
+let firstCard = 10
+let secondCard = 12
+let sum = firstCard + secondCard;
 
+if(sum < 21){
+  console.log("do you want to draw another card  ? 😊")
+}
+else if (sum === 21){
+  console.log("wohoo! Yo've got Blackjack! 🥳");
+}
+else {
+  console.log("You're out of the game. 😭")
+}
