d8761891b8470cb879c41bd126ff3ad65acb75a7
nitish-pardal <nitishpardal@gmail.com>
2023-11-01 10:25:46 +0530

    01-11-2023

--- learn-javascript-main/1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
@@ -4,4 +4,7 @@
 // Create a third variable, myGreeting, that contatenates the two strings
 // Log myGreeting to the console
 
-
+let name = "Nitish Pardal";
+let greetings= "Hi, my name is ";
+let concatenated = greetings + name;
+console.log(concatenated);
