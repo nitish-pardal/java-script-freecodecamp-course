56c9567c413f75d15c6d731c436c50fa0429ee68
nitish-pardal <nitishpardal@gmail.com>
2023-11-02 16:28:04 +0530

    02-11-2023

--- learn-javascript-main/3. Build a Blackjack game/09. Let's practice boolean conditions/index.js
+++ learn-javascript-main/3. Build a Blackjack game/09. Let's practice boolean conditions/index.js
@@ -2,7 +2,7 @@ console.log(4 === 3)  // false
 console.log(5 > 2)    // true
 console.log(12 > 12)  // false
 console.log(3 < 0)    // false
-console.log(3 >= 3)   //  true
+console.log(3 >= 3)   // true
 console.log(11 <= 11) // true
 console.log(3 <= 2)   // false
 
--- learn-javascript-main/3. Build a Blackjack game/10. Add the message variable/index.js
+++ learn-javascript-main/3. Build a Blackjack game/10. Add the message variable/index.js
@@ -3,17 +3,18 @@ let secondCard = 11
 let sum = firstCard + secondCard + 4
 let hasBlackJack = false
 let isAlive = true
+let message = "";
 // 1. Declare a variable called message and assign its value to an empty string
 
 // 2. Reassign the message variable to the string we're logging out
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message ="Do you want to draw a new card? 🙂";
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳";
     hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭";
     isAlive = false
 }
-
+console.log(message);
 // 3. Log it out!
