99a6636f21ce8995bf5a4bac834e1fe85484cc6e
nitish-pardal <nitishpardal@gmail.com>
2023-11-02 18:38:15 +0530

    02-11-2023

--- learn-javascript-main/3. Build a Blackjack game/17. New card button/index.js
+++ learn-javascript-main/3. Build a Blackjack game/17. New card button/index.js
@@ -6,7 +6,8 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let cardsEl = document.getElementById("cards-el");
+
 
 function startGame() {
     cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
@@ -22,5 +23,12 @@ function startGame() {
     }
     messageEl.textContent = message
 }
+
+
+document.getElementById('new-card').addEventListener('click',newCard);
+
+function newCard (){
+    console.log("Drawing a new card from the deck!");
+};
 
 // 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
