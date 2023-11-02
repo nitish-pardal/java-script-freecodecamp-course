01e5db10e554ae23d09b93cadaaa0d7d596f690a
nitish-pardal <nitishpardal@gmail.com>
2023-11-02 14:59:03 +0530

    02-11-2023

--- learn-javascript-main/3. Build a Blackjack game/08. Add the isAlive variable/index.js
+++ learn-javascript-main/3. Build a Blackjack game/08. Add the isAlive variable/index.js
@@ -1,8 +1,9 @@
 let firstCard = 10
 let secondCard = 11
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard +4 ;
 let hasBlackJack = false
 // 1. Create a variable called isAlive and assign it to true
+let isAlive = true;
 
 // 2. Flip its value to false in the appropriate code block 
 if (sum <= 20) {
@@ -11,7 +12,9 @@ if (sum <= 20) {
     console.log("Wohoo! You've got Blackjack! 🥳")
     hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    console.log("You're out of the game! 😭");
+    isAlive = false;
 }
 
+console.log(isAlive);
 // 3. Log it out to check that you're doing it right
