d8ae2a1bd2515fcc35b270229731029576e67252
nitish-pardal <nitishpardal@gmail.com>
2023-11-05 14:10:14 +0530

    05-11-2023

--- learn-javascript-main/5. Build a Chrome Extension/04. Make the input button work with onclick/index.js
+++ learn-javascript-main/5. Build a Chrome Extension/04. Make the input button work with onclick/index.js
@@ -1,1 +1,7 @@
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+let buttonEl = document.querySelector('#input-btn');
+buttonEl.addEventListener('click',saveLead);
+
+function saveLead (){
+console.log('button Clicked!');
+}
